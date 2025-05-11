fc2f72cea804b96e687d1dc3fb4d181e84efdbaf
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2025-05-11 17:49:11 +0200

    Add files via upload

---
+++ quotes-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import QuotesApp from "./components/QuotesApp/QuotesApp";
+
+const App = () => {
+  return (
+    <div>
+      <QuotesApp />
+    </div>
+  );
+};
+
+export default App;
